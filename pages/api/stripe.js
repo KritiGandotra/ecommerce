b273f037dda82ee071f8818bb0aef94f747500c2
Kriti Gandotra <kritigandotra@gmail.com>
2023-08-06 12:52:08 +0530

    first commit

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
           };
         }),
         mode: "payment",
-        success_url: `${req.headers.origin}/success=true`,
+        success_url: `${req.headers.origin}/success`,
         cancel_url: `${req.headers.origin}/canceled=true`,
       };
       const session = await stripe.checkout.sessions.create(params);
